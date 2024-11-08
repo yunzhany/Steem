b332513e98b7c14bd76d3046a73d3f662cb7998d
yunzhany <69857969+yunzhany@users.noreply.github.com>
2024-11-08 11:54:33 -0600

    Maven setup test

---
+++ src/main/java/com/github/yunzhany/Demo.java
@@ -0,0 +1,10 @@
+package com.github.yunzhany;
+
+public class Demo {
+    public String sayHello(String name) {
+        return "Hello " + name;
+    }
+
+    public static void main(String[] args) {
+    }
+}
---
+++ src/test/java/com/github/yunzhany/DemoTest.java
@@ -0,0 +1,11 @@
+package com.github.yunzhany;
+
+import org.junit.Test;
+
+public class DemoTest {
+    @Test
+    public void testDay () {
+        Demo demo = new Demo();
+        System.out.println(demo.sayHello("Andrew"));
+    }
+}
