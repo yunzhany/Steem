807f007edcf1b8eb50702fdef1bd5371ad0cecd6
yunzhany <69857969+yunzhany@users.noreply.github.com>
2024-11-08 11:35:14 -0600

    Maven set up

--- src/main/java/com/github/yunzhany/ExampleClass.java
+++ src/main/java/com/github/yunzhany/ExampleClass.java
@@ -1,7 +1,10 @@
+package com.github.yunzhany;
+
 public class ExampleClass {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("example change");
+        System.out.println("Changed directory name");
 
     }
 }
